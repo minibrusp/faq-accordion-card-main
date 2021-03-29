f49858a7539f32bc94199c83dbf4bd086000ecb2
Bruce S. Perez <minibrusp@gmail.com>
2021-03-29 16:51:56 +0800

    Added JS accordion feature

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+'use strict';
+
+let question = document.querySelectorAll('.accordion__question');
+
+question.forEach(question => {
+  console.log(question);
+  question.addEventListener('click', () => {
+    question.classList.toggle('accordion__question__show:after');
+    question.classList.toggle('accordion__question__show');
+    question.nextElementSibling.classList.toggle('accordion__collapse__show');
+    console.log(`is clicked question: ${question.innerHTML}`);
+  });
+});
