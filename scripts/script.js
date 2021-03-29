7fa142595c4534e53fee26b7aada277d886af5a2
Bruce S. Perez <minibrusp@gmail.com>
2021-03-29 17:31:31 +0800

    Added 1 per click item accordion feature but cant close if click on opened accordion

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,22 @@ let question = document.querySelectorAll('.accordion__question');
 question.forEach(question => {
   console.log(question);
   question.addEventListener('click', () => {
+    checkerShow();
     question.classList.toggle('accordion__question__show:after');
     question.classList.toggle('accordion__question__show');
     question.nextElementSibling.classList.toggle('accordion__collapse__show');
     console.log(`is clicked question: ${question.innerHTML}`);
   });
 });
+
+
+function checkerShow() {
+  question.forEach(question => {
+    if(question.classList.contains('accordion__question__show') === true) {
+      console.log("Contains");
+      question.classList.toggle('accordion__question__show:after');
+      question.classList.toggle('accordion__question__show');
+      question.nextElementSibling.classList.toggle('accordion__collapse__show');
+    }
+  });
+}
