9f0dbfa623a8267962eee5f88d184228ecefb85a
Bruce S. Perez <minibrusp@gmail.com>
2021-03-29 23:55:27 +0800

    fixed accordion 1 drawer open at a time

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,76 @@
 'use strict';
 
+// let question = document.getElementsByClassName('accordion__question');
 let question = document.querySelectorAll('.accordion__question');
 
-question.forEach(question => {
-  console.log(question);
-  question.addEventListener('click', () => {
-    checkerShow();
-    question.classList.toggle('accordion__question__show:after');
-    question.classList.toggle('accordion__question__show');
-    question.nextElementSibling.classList.toggle('accordion__collapse__show');
-    console.log(`is clicked question: ${question.innerHTML}`);
-  });
-});
+
+//ver#1 using normal anonymous function with this
+// for(let q of question) {
+//   q.addEventListener('click', function() {
+//     for(let e of question) {
+//       if(e != this) {
+//         e.classList.remove('accordion__question__show');
+//         e.nextElementSibling.classList.remove('accordion__collapse__show');
+//       }
+//       else {
+//         this.classList.toggle('accordion__question__show');
+//         this.nextElementSibling.classList.toggle('accordion__collapse__show');
+//       }
+//     }
+//   });
+// }
+
+// ver#2 using anonymous arrowed function cant use this dont know why
+
+// for(let q of question) {
+//   q.addEventListener('click', () => {
+//     for(let e of question) {
+//       if(e != q) {
+//         e.classList.remove('accordion__question__show');
+//         e.nextElementSibling.classList.remove('accordion__collapse__show');
+//       }
+//       else {
+//         e.classList.toggle('accordion__question__show');
+//         e.nextElementSibling.classList.toggle('accordion__collapse__show');
+//       }
+//     }
+//   });
+// }
 
 
-function checkerShow() {
-  question.forEach(question => {
-    if(question.classList.contains('accordion__question__show') === true) {
-      console.log("Contains");
-      question.classList.toggle('accordion__question__show:after');
-      question.classList.toggle('accordion__question__show');
-      question.nextElementSibling.classList.toggle('accordion__collapse__show');
+// ver#3 
+
+// const clickHandler = (event) => {
+//   for(let h of question){
+//     if(h != event.currentTarget) {
+//       console.log('Not target')
+//       h.classList.remove('accordion__question__show');
+//       h.nextElementSibling.classList.remove('accordion__collapse__show');
+//     }
+//     else {
+//       event.currentTarget.classList.toggle('accordion__question__show');
+//       event.currentTarget.nextElementSibling.classList.toggle('accordion__collapse__show');
+//     }
+//   }
+// }
+
+// for (let q of question) {
+//   q.addEventListener('click', clickHandler);
+// }
+
+// #ver 4 
+
+for(let q of question) {
+  q.addEventListener('click', (event) => {
+    for(let e of question) {
+      if(e != q) {
+        e.classList.remove('accordion__question__show');
+        e.nextElementSibling.classList.remove('accordion__collapse__show');
+      }
+      else {
+        event.currentTarget.classList.toggle('accordion__question__show');
+        event.currentTarget.nextElementSibling.classList.toggle('accordion__collapse__show');
+      }
     }
   });
 }
