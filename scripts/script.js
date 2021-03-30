25438dba616534306182c997f8856431d70c1121
Bruce S. Perez <minibrusp@gmail.com>
2021-03-30 16:20:39 +0800

    Added Content Loaded

--- scripts/script.js
+++ scripts/script.js
@@ -1,76 +1,78 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', (event) => {
+  'use strict';
 
-// let question = document.getElementsByClassName('accordion__question');
-let question = document.querySelectorAll('.accordion__question');
+  // let question = document.getElementsByClassName('accordion__question');
+  let question = document.querySelectorAll('.accordion__question');
+
+  //ver#1 
+  // for(let q of question) {
+  //   q.addEventListener('click', function() {
+  //     for(let e of question) {
+  //       if(e != this) {
+  //         e.classList.remove('accordion__question__show');
+  //         e.nextElementSibling.classList.remove('accordion__collapse__show');
+  //       }
+  //       else {
+  //         this.classList.toggle('accordion__question__show');
+  //         this.nextElementSibling.classList.toggle('accordion__collapse__show');
+  //       }
+  //     }
+  //   });
+  // }
+
+  // ver#2 
+
+  // for(let q of question) {
+  //   q.addEventListener('click', () => {
+  //     for(let e of question) {
+  //       if(e != q) {
+  //         e.classList.remove('accordion__question__show');
+  //         e.nextElementSibling.classList.remove('accordion__collapse__show');
+  //       }
+  //       else {
+  //         e.classList.toggle('accordion__question__show');
+  //         e.nextElementSibling.classList.toggle('accordion__collapse__show');
+  //       }
+  //     }
+  //   });
+  // }
 
 
-//ver#1 
-// for(let q of question) {
-//   q.addEventListener('click', function() {
-//     for(let e of question) {
-//       if(e != this) {
-//         e.classList.remove('accordion__question__show');
-//         e.nextElementSibling.classList.remove('accordion__collapse__show');
-//       }
-//       else {
-//         this.classList.toggle('accordion__question__show');
-//         this.nextElementSibling.classList.toggle('accordion__collapse__show');
-//       }
-//     }
-//   });
-// }
+  // ver#3 
 
-// ver#2 
+  // const clickHandler = (event) => {
+  //   for(let h of question){
+  //     if(h != event.currentTarget) {
+  //       console.log('Not target')
+  //       h.classList.remove('accordion__question__show');
+  //       h.nextElementSibling.classList.remove('accordion__collapse__show');
+  //     }
+  //     else {
+  //       event.currentTarget.classList.toggle('accordion__question__show');
+  //       event.currentTarget.nextElementSibling.classList.toggle('accordion__collapse__show');
+  //     }
+  //   }
+  // }
 
-// for(let q of question) {
-//   q.addEventListener('click', () => {
-//     for(let e of question) {
-//       if(e != q) {
-//         e.classList.remove('accordion__question__show');
-//         e.nextElementSibling.classList.remove('accordion__collapse__show');
-//       }
-//       else {
-//         e.classList.toggle('accordion__question__show');
-//         e.nextElementSibling.classList.toggle('accordion__collapse__show');
-//       }
-//     }
-//   });
-// }
+  // for (let q of question) {
+  //   q.addEventListener('click', clickHandler);
+  // }
 
+  // #ver 4 
 
-// ver#3 
-
-// const clickHandler = (event) => {
-//   for(let h of question){
-//     if(h != event.currentTarget) {
-//       console.log('Not target')
-//       h.classList.remove('accordion__question__show');
-//       h.nextElementSibling.classList.remove('accordion__collapse__show');
-//     }
-//     else {
-//       event.currentTarget.classList.toggle('accordion__question__show');
-//       event.currentTarget.nextElementSibling.classList.toggle('accordion__collapse__show');
-//     }
-//   }
-// }
-
-// for (let q of question) {
-//   q.addEventListener('click', clickHandler);
-// }
-
-// #ver 4 
-
-for(let q of question) {
-  q.addEventListener('click', (event) => {
-    for(let e of question) {
-      if(e != q) {
-        e.classList.remove('accordion__question__show');
-        e.nextElementSibling.classList.remove('accordion__collapse__show');
+  for(let q of question) {
+    q.addEventListener('click', (event) => {
+      for(let e of question) {
+        if(e != q) {
+          e.classList.remove('accordion__question__show');
+          e.nextElementSibling.classList.remove('accordion__collapse__show');
+        }
+        else {
+          event.currentTarget.classList.toggle('accordion__question__show');
+          event.currentTarget.nextElementSibling.classList.toggle('accordion__collapse__show');
+        }
       }
-      else {
-        event.currentTarget.classList.toggle('accordion__question__show');
-        event.currentTarget.nextElementSibling.classList.toggle('accordion__collapse__show');
-      }
-    }
-  });
-}
+    });
+  }
+    console.log('DOM fully loaded and parsed');
+});
